f4a160b7599467ad4171c7f6b6816dfa07e662f9
Eli RoCas <elir_94@hotmail.com>
2023-09-09 10:19:35 -0500

    Ejercicio2-Interpolación-ternarios

--- 5. Talleres Algoritmos y JavaScript/js/eje2.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje2.js
@@ -1,13 +1,18 @@
 document.getElementById('enunciado--eje2').innerHTML = "Haga el algoritmo que, dada una variable cuyo valor es leído desde el teclado, imprima por pantalla si el número es o no mayor que 0 (cero)."
 
-let valor1;
-valor1 = parseInt(prompt('Ingrese un número'))
-if (valor1 > 0) {
-    document.getElementById('resultado--eje2').innerHTML = "El número ingresado es mayor que cero. Sú número es " + valor1
+let valor1 = parseInt(prompt('Ingrese un número'));
 
-} else {
-    document.getElementById('resultado--eje2').innerHTML = "El número ingresado no es mayor que cero. Sú número es " + valor1
 
-}
+// Forma sin interpolación y operadores ternarios 
+//if (valor1 > 0) {
+//     document.getElementById('resultado--eje2').innerHTML = "El número ingresado es mayor que cero. Sú número es " + valor1
+
+// } else {
+//     document.getElementById('resultado--eje2').innerHTML = "El número ingresado no es mayor que cero. Sú número es " + valor1
+// }
+
+document.getElementById('resultado--eje2').innerHTML = `El número ingresado es ${valor1 > 0
+    ? `mayor que cero.`
+    : `menor que cero.`} Su número es ${valor1}`;
 
 
