76d56454780b8e038eeb4c9ced396922868e2ef4
Eli Rosero <elir_94@hotmail.com>
2023-09-11 22:11:05 -0500

    Ejercicio 4 - Switch

--- 5. Talleres Algoritmos y JavaScript/js/eje4.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje4.js
@@ -1,15 +1,29 @@
 document.getElementById('enunciado--eje4').innerHTML="Determine si un viajero de una Aerolínea puede tomar un tiquete determinado en clase: Ejecutiva, Normal o Económica. Dependiendo de los puntos que ten a puede tener acceso a cada clase. Más de 2000 puntos puede estar en Ejecutiva, entre 1000 y 1999 puede estar en Normal y menos de 1000 puede estar en Económica.";
 
-let num_puntos;
+let num_puntos = parseInt(prompt('Ingrese la cantidad de puntos que posee'));
+let solt = document.getElementById('resultado--eje4');
 
-num_puntos = parseInt(prompt('Ingrese la cantidad de puntos que posee'));
+//solt.innerHTML=`Puede estar en clase ${num_puntos>=2000 ? "Ejecutiva" : "Normal"} `;
 
-if (num_puntos >= 2000) {
-    document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Ejecutiva ";
+switch (true){
+    case num_puntos >= 2000: 
+        solt.innerHTML="Puede estar en clase Ejecutiva";
+    break;
+    case num_puntos < 2000 && num_puntos >= 1000: 
+        solt.innerHTML="Puede estar en clase Normal"; 
+    break;
+    case num_puntos <1000:
+        solt.innerHTML="Puede estar en clase Económica"; 
+    break;
 }
-else if (num_puntos < 2000 && num_puntos >= 1000) {
-    document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Normal";
-}
-else if (num_puntos < 1000) {
-    document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Económica";
-}
+
+
+// if (num_puntos >= 2000) {
+//     document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Ejecutiva ";
+// }
+// else if (num_puntos < 2000 && num_puntos >= 1000) {
+//     document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Normal";
+// }
+// else if (num_puntos < 1000) {
+//     document.getElementById('resultado--eje4').innerHTML="Puede estar en clase Económica";
+// }
