cecc4794883c2a8465182e8114642bb0c31859d1
Eli Rosero <elir_94@hotmail.com>
2023-09-12 11:46:28 -0500

    Ejercicio6-Inerpolación-y-ternario

--- 5. Talleres Algoritmos y JavaScript/js/eje6.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje6.js
@@ -1,16 +1,27 @@
 document.getElementById('enunciado--eje6').innerHTML = "Diseñe un algoritmo que calcule la nota final de un estudiante con los siguientes porcentajes: primer parcial 30%, segundo parcial 30% y examen final 40%. Al final determine si pudo aprobar o no la asignatura. La nota mínima aprobatoria es 3.2 (Tres punto Dos).";
 
-let nota_final, nota_parcial1, nota_parcial2, nota_parcial3;
+let nota_parcial1 = parseFloat(prompt('Ingrese la nota de su primer parcial'));
+let nota_parcial2 = parseFloat(prompt('Ingrese la nota de su segundo parcial'));
+let nota_parcial3 = parseFloat(prompt('Ingrese la nota de su examen final'));
+let nota_final = (nota_parcial1 * 0.3 + nota_parcial2 * 0.3 + nota_parcial3 * 0.4);
+const nota=document.getElementById('resultado--eje6');
 
-nota_parcial1 = parseFloat(prompt('Ingrese la nota de su primer parcial'));
-nota_parcial2 = parseFloat(prompt('Ingrese la nota de su segundo parcial'));
-nota_parcial3 = parseFloat(prompt('Ingrese la nota de su examen final'));
+nota.innerHTML=`Su nota final es ${nota_final} ${nota_final>=3.2 ? '¡Felicitaciones, aprobó!' : 'Reprobó'}`;
 
-nota_final = (nota_parcial1 * 0.3 + nota_parcial2 * 0.3 + nota_parcial3 * 0.4);
 
-if (nota_final >= 3.2) {
-    document.getElementById('resultado--eje6').innerHTML = "Felicitaciones, aprobó. Su nota final es: " + nota_final;
-}
-else {
-    document.getElementById('resultado--eje6').innerHTML = "Reprobó. Su nota final es: " + nota_final;
-}
+
+//Forma inicial 
+// let nota_final, nota_parcial1, nota_parcial2, nota_parcial3;
+
+// nota_parcial1 = parseFloat(prompt('Ingrese la nota de su primer parcial'));
+// nota_parcial2 = parseFloat(prompt('Ingrese la nota de su segundo parcial'));
+// nota_parcial3 = parseFloat(prompt('Ingrese la nota de su examen final'));
+
+// nota_final = (nota_parcial1 * 0.3 + nota_parcial2 * 0.3 + nota_parcial3 * 0.4);
+
+// if (nota_final >= 3.2) {
+//     document.getElementById('resultado--eje6').innerHTML = "Felicitaciones, aprobó. Su nota final es: " + nota_final;
+// }
+// else {
+//     document.getElementById('resultado--eje6').innerHTML = "Reprobó. Su nota final es: " + nota_final;
+// }
