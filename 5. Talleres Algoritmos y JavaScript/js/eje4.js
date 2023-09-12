941e577ee60dd47deb2b9e2c386fde109c30b665
Eli Rosero <elir_94@hotmail.com>
2023-09-12 11:23:52 -0500

    Ejercicio4-Interpolación-ternarios-2-expresiones

--- 5. Talleres Algoritmos y JavaScript/js/eje4.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje4.js
@@ -1,21 +1,21 @@
-document.getElementById('enunciado--eje4').innerHTML="Determine si un viajero de una Aerolínea puede tomar un tiquete determinado en clase: Ejecutiva, Normal o Económica. Dependiendo de los puntos que ten a puede tener acceso a cada clase. Más de 2000 puntos puede estar en Ejecutiva, entre 1000 y 1999 puede estar en Normal y menos de 1000 puede estar en Económica.";
+document.getElementById('enunciado--eje4').innerHTML = "Determine si un viajero de una Aerolínea puede tomar un tiquete determinado en clase: Ejecutiva, Normal o Económica. Dependiendo de los puntos que ten a puede tener acceso a cada clase. Más de 2000 puntos puede estar en Ejecutiva, entre 1000 y 1999 puede estar en Normal y menos de 1000 puede estar en Económica.";
 
 let num_puntos = parseInt(prompt('Ingrese la cantidad de puntos que posee'));
 let solt = document.getElementById('resultado--eje4');
 
-//solt.innerHTML=`Puede estar en clase ${num_puntos>=2000 ? "Ejecutiva" : "Normal"} `;
+solt.innerHTML=`Puede estar en clase ${num_puntos>=2000 ? "Ejecutiva" : (num_puntos < 2000 && num_puntos >= 1000) ? 'Normal': 'Eco'} `;
 
-switch (true){
-    case num_puntos >= 2000: 
-        solt.innerHTML="Puede estar en clase Ejecutiva";
-    break;
-    case num_puntos < 2000 && num_puntos >= 1000: 
-        solt.innerHTML="Puede estar en clase Normal"; 
-    break;
-    case num_puntos <1000:
-        solt.innerHTML="Puede estar en clase Económica"; 
-    break;
-}
+// switch (true) {
+//     case num_puntos >= 2000:
+//         solt.innerHTML = "Puede estar en clase Ejecutiva";
+//         break;
+//     case num_puntos < 2000 && num_puntos >= 1000:
+//         solt.innerHTML = "Puede estar en clase Normal";
+//         break;
+//     case num_puntos < 1000:
+//         solt.innerHTML = "Puede estar en clase Económica";
+//         break;
+// }
 
 
 // if (num_puntos >= 2000) {
--- 5. Talleres Algoritmos y JavaScript/js/eje5.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje5.js
@@ -1,16 +1,28 @@
 document.getElementById('enunciado--eje5').innerHTML = "Escriba un algoritmo que intercambie el valor de dos variables numéricas sólo si el valor de la primera es mayor que el de la segunda.";
 
-let num_a;
-let num_b;
-
-num_a = parseInt(prompt('Ingrese el valor de a '));
-num_b = parseInt(prompt('Ingrese el valor de b'));
-
+let num_a = parseInt(prompt('Ingrese el valor de a '));
+let num_b = parseInt(prompt('Ingrese el valor de b'));
+let temp;
+let final= document.getElementById('resultado--eje5'); 
 
 if (num_a > num_b) { 
-    let temp=num_a;
-    num_a=num_b;
-    num_b=temp;
-    document.getElementById('resultado--eje5').innerHTML= "El valor a fue mayor que el valor b. Valores intercambiados: " + num_a + " " + num_b;
+    [num_a, num_b] = [num_b, num_a]
+    final.innerHTML= "El valor a fue mayor que el valor b. Valores intercambiados: a= " + num_a + " b=" + num_b;
 }
-else document.getElementById('resultado--eje5').innerHTML= "El valor a fue menor que el valor b. Valores no intercambiados: " + num_a + " " + num_b;
+else final.innerHTML= "El valor a fue menor que el valor b. Valores no intercambiados: a=" + num_a + "b= " + num_b;
+
+
+// if (num_a > num_b) { 
+//     let temp=num_a;
+//     num_a=num_b;
+//     num_b=temp;
+//     document.getElementById('resultado--eje5').innerHTML= "El valor a fue mayor que el valor b. Valores intercambiados: " + num_a + " " + num_b;
+// }
+// else document.getElementById('resultado--eje5').innerHTML= "El valor a fue menor que el valor b. Valores no intercambiados: " + num_a + " " + num_b;
+
+
+// Con interpolación, ternarios y desestructuración de arreglos - No funciona el mensaje completo 
+//final.innerHTML=`El valor a fue ${num_a> num_b ? "mayor a b" [num_a, num_b]=[num_b, num_a] 
+//: "menor que b. Valores no intercambiados"}`;
+
+
