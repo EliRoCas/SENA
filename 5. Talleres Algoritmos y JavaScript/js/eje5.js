27643a4860e80c6f81a5f205b92de304ec47d9e8
Eli RoCas <elir_94@hotmail.com>
2023-09-12 20:07:30 -0500

    EA

--- 5. Talleres Algoritmos y JavaScript/js/eje5.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje5.js
@@ -2,15 +2,17 @@ document.getElementById('enunciado--eje5').innerHTML = "Escriba un algoritmo que
 
 let num_a = parseInt(prompt('Ingrese el valor de a '));
 let num_b = parseInt(prompt('Ingrese el valor de b'));
-let temp;
+//let temp;
 let final = document.getElementById('resultado--eje5');
 
 // Con desestructuración de arreglos, para intercambiar los valores de a y b 
 if (num_a > num_b) {
-    [num_a, num_b] = [num_b, num_a]
+    [num_a, num_b] = [num_b, num_a];
     final.innerHTML = "El valor a fue mayor que el valor b. Valores intercambiados: a= " + num_a + " b=" + num_b;
 }
-else final.innerHTML = "El valor a fue menor que el valor b. Valores no intercambiados: a=" + num_a + "b= " + num_b;
+else {
+    final.innerHTML = "El valor a fue menor que el valor b. Valores no intercambiados: a=" + num_a + "b= " + num_b;
+}
 
 
 // if (num_a > num_b) {
