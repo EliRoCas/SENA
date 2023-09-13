eb5029b402cf7ca9e898069bde0d5791d4eceee1
Eli RoCas <elir_94@hotmail.com>
2023-09-12 21:25:02 -0500

    Ejercicio12-interpolación-destructuración-de-arreg

--- 5. Talleres Algoritmos y JavaScript/js/eje12.js
+++ 5. Talleres Algoritmos y JavaScript/js/eje12.js
@@ -3,27 +3,32 @@ const result12 = document.getElementById('resultado--eje12');
 
 enunciado12.innerHTML = "Solicitar tres números al usuario e intercambiarlos entre si, solo si el segundo es mayor que el primero y el tercero.";
 
-let num_aa;
-let num_bb;
-let num_cc;
-
-num_aa = parseInt(prompt('Ingrese el valor de a '));
-num_bb = parseInt(prompt('Ingrese el valor de b'));
-num_cc = parseInt(prompt('Ingrese el valor de c'));
+let num_aa = parseInt(prompt('Ingrese el valor de a '));
+let num_bb = parseInt(prompt('Ingrese el valor de b'));
+let num_cc = parseInt(prompt('Ingrese el valor de c'));
 
 if (num_bb > num_aa && num_bb > num_cc) {
-    let temp = num_bb;
-    num_bb = num_aa;
-    num_aa = num_cc;
-    num_cc = temp;
-    result12.innerHTML = "El valor de b es mayor que el valor de a y de c. Valores intercambiados: "
-        + num_aa
-        + " "
-        + num_bb
-        + " "
-        + num_cc;
+    [num_aa, num_bb, num_cc] = [num_cc, num_aa, num_bb];
+    result12.innerHTML = `El valor de b es mayor que el valor de a y de c. Valores intercambiados: ${num_aa} ${num_bb} ${num_cc}`;
 }
 else {
-    result12.innerHTML = "El valor de b es menor que alguno de los dos valores (a - c). Valores no intercambiados" + num_aa + " " + num_bb + " " + num_cc;
+    result12.innerHTML = `El valor de b es menor que alguno de los dos valores (a - c). Valores no intercambiados ${num_aa} ${num_bb} ${num_cc}`;
 }
+
+
+// if (num_bb > num_aa && num_bb > num_cc) {
+//     let temp = num_bb;
+//     num_bb = num_aa;
+//     num_aa = num_cc;
+//     num_cc = temp;
+//     result12.innerHTML = "El valor de b es mayor que el valor de a y de c. Valores intercambiados: "
+//         + num_aa
+//         + " "
+//         + num_bb
+//         + " "
+//         + num_cc;
+// }
+// else {
+//     result12.innerHTML = "El valor de b es menor que alguno de los dos valores (a - c). Valores no intercambiados" + num_aa + " " + num_bb + " " + num_cc;
+// }
 
