9909b361b3dbc290d7ba06a1e3d51bf9d30aba17
Eli Rosero <elir_94@hotmail.com>
2023-09-14 14:42:07 -0500

    taller02

---
+++ Taller JS 2/taller02.js
@@ -0,0 +1,13 @@
+//Ejercicio 05 
+
+function ejer05(){
+    document.getElementById('enunciado').innerHTML=`
+    <p>Elabore un algoritmo que lea un número negativo e imprima el 
+    número y el positivo del mismo.</p>
+    `;
+}
+
+function positivo(){
+    let num = prompt{'Ingrese número negativo'};
+    num
+}
