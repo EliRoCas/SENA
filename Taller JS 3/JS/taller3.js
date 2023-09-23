cf8e6875df6494972afe6f531f59828bb767ace8
Eli Rosero <elir_94@hotmail.com>
2023-09-23 17:00:20 -0500

    Taller 03 JS

--- Taller JS 3/JS/taller3.js
+++ Taller JS 3/JS/taller3.js
@@ -18,16 +18,35 @@ function ejem01() {
 // Array con números aleatorios, suma de esos números y su media. 
 function ejem02() {
     let numeros02 = [];
-    while (numeros02.length < 10) {
-        let numAleatorio = Math.floor(Math.random()*50);
+    while (numeros02.length <= 10) {
+        let numAleatorio = Math.floor(Math.random() * 50);
         numeros02.push(numAleatorio);
     }
     let suma = 0;
-    for (let sum = 0; sum < numeros02.length; sum++){
-        suma+=numeros02[sum];
+    for (let sum = 0; sum < numeros02.length; sum++) {
+        suma += numeros02[sum];
     }
     let media = suma / numeros02.length;
-    document.getElementById('resultado').innerHTML=`Los 10 números son: ${numeros02}. Su media es: ${media}.`;
+    document.getElementById('resultado').innerHTML = `Los 10 números son: ${numeros02}. Su media es: ${media}.`;
 
 }
 
+function ejem03() {
+    let longitudArray = parseInt(prompt('Ingrese el tamaño que debe tener el array.'));
+    let numeroBase = parseInt(prompt('Ingrese el número del que desea optener los múltiplos.'));
+    function arrayRelleno() {
+        let numeros03 = [];
+        let multiplo = numeroBase;
+        while (numeros03.length < longitudArray) {
+            numeros03.push(multiplo);
+            multiplo += numeroBase;
+        }
+       return numeros03;
+    }
+   let resultado = arrayRelleno();
+    document.getElementById('resultado').innerHTML=`El número ingresado fue ${numeroBase}, sus múltiplos son ${resultado.join(', ')}.`;
+}
+
+function ejem04(){
+    
+}
