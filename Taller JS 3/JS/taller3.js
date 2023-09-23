c02d980c2fb543f0bfd427bd3308c7f033c7a2c0
Eli Rosero <elir_94@hotmail.com>
2023-09-23 16:08:47 -0500

    Taller 03 JS

--- Taller JS 3/JS/taller3.js
+++ Taller JS 3/JS/taller3.js
@@ -6,19 +6,28 @@ function mostrar(dato) {
     document.getElementById('ejemplo' + dato).style.display = "block";
 }
 
+// Array con 5 números
 function ejem01() {
     let numeros01 = [64, 56, 37, 34, 29];
-    console.log(numeros);
+    console.log(numeros01);
     for (let num = 0; num <= numeros01.length; num++) {
         document.getElementById('resultado').innerHTML = `Estos son los 5 números almacenados ${numeros01}`;
     }
 }
 
+// Array con números aleatorios, suma de esos números y su media. 
 function ejem02() {
     let numeros02 = [];
     while (numeros02.length < 10) {
-        let numAleatorio = Math.random
+        let numAleatorio = Math.floor(Math.random()*50);
+        numeros02.push(numAleatorio);
     }
+    let suma = 0;
+    for (let sum = 0; sum < numeros02.length; sum++){
+        suma+=numeros02[sum];
+    }
+    let media = suma / numeros02.length;
+    document.getElementById('resultado').innerHTML=`Los 10 números son: ${numeros02}. Su media es: ${media}.`;
 
 }
 
