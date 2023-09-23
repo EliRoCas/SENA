9f83ccc355328c9770483b608cc86eac5a38da59
Eli Rosero <elir_94@hotmail.com>
2023-09-23 14:01:12 -0500

    Taller 03 JS

---
+++ Taller JS 3/JS/taller3.js
@@ -0,0 +1,67 @@
+function mostrar(dato) {
+    switch (dato) {
+        case 1:
+            document.getElementById('ejemplo2').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 2:
+            document.getElementById('ejemplo2').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 3:
+            document.getElementById('ejemplo3').style.display = "block";
+            document.getElementById('ejemplo2').style.display = "none";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 4:
+            document.getElementById('ejemplo4').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo2').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 5:
+            document.getElementById('ejemplo5').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo2').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 6:
+            document.getElementById('ejemplo6').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo2').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo7').style.display = "none";
+            break;
+        case 7:
+            document.getElementById('ejemplo7').style.display = "block";
+            document.getElementById('ejemplo1').style.display = "none";
+            document.getElementById('ejemplo2').style.display = "none";
+            document.getElementById('ejemplo3').style.display = "none";
+            document.getElementById('ejemplo4').style.display = "none";
+            document.getElementById('ejemplo5').style.display = "none";
+            document.getElementById('ejemplo6').style.display = "none";
+            break;
+    }
+}
